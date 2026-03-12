a387fe23384477567172b19eb90ca0c1b4ab49fd
Dawid Budz <150971023+budzskl@users.noreply.github.com>
2026-03-11 20:25:54 -0500

    fix bash scripts

--- backend.sh
+++ backend.sh
@@ -8,6 +8,6 @@ python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 
-  cd app && cd ..
+cd app && cd ..
 
 flask --app app/server.py --debug run
--- initial-backend.sh
+++ initial-backend.sh
@@ -7,6 +7,6 @@ python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 
-  cd app && python init_db.py && cd ..
+cd app && python init_db.py && cd ..
 
 flask --app app/server.py --debug run
